52639398a1c54b10a5fc2a469fef267d27626fd1
lgautier <none@none>
2008-03-03 21:43:24 +0000

    Added a README Added a file to demo a bit (this will/should go away when doc appears)
    --HG--
    branch : rpy_nextgen

---
+++ test.py
@@ -0,0 +1,40 @@
+"""
+short demo.
+
+Running this through ipython ends in a memory map
+dump and a segfault.
+"""
+
+import robjects
+import array
+
+r = robjects.r
+
+x = array.array('i', range(10))
+y = r.rnorm(10)
+
+r.X11()
+
+r.par(mfrow=array.array('i', [2,2]))
+r.plot(x, y, ylab="foo/bar", col="red")
+
+kwargs = {'ylab':"foo/bar", 'type':"b", 'col':"blue", 'log':"x"}
+r.plot(x, y, **kwargs)
+
+
+m = r.matrix(r.rnorm(100), ncol=5)
+pca = r.princomp(m)
+r.plot(pca, main="Eigen values")
+r.biplot(pca, main="biplot")
+
+
+if not r.require("GO")[0]:
+    raise(Exception("Bioconductor Package GO missing"))
+
+
+goItem = r.GOTERM["GO:0000001"]
+
+## goItem in an instance of an S4 class
+## slots can be accessed as Python attributes
+goItem.Term
+
