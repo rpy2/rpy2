254c62b61d84f2affaddde336cc0b57e84cfc10f
lgautier <none@none>
2008-05-21 20:15:07 +0000

    Added missing new test file
    --HG--
    branch : rpy_nextgen

---
+++ rpy/robjects/tests/testRArray.py
@@ -0,0 +1,29 @@
+import unittest
+import rpy2.robjects as robjects
+rinterface = robjects.rinterface
+import array
+
+class RArrayTestCase(unittest.TestCase):
+
+    def testNew(self):
+        letters = robjects.r.letters        
+        self.assertRaises(TypeError, robjects.RArray, letters)
+        m = robjects.r.matrix(1, nrow=5, ncol=3)
+        a = robjects.RArray(m)
+        
+    def testDim(self):
+        m = robjects.r.matrix(1, nrow=5, ncol=3)
+        a = robjects.RArray(m)
+        d = a.dim
+        self.assertEquals(2, len(d))
+        self.assertEquals(5, d[0])
+        self.assertEquals(3, d[1])
+
+
+
+def suite():
+    suite = unittest.TestLoader().loadTestsFromTestCase(RArrayTestCase)
+    return suite
+
+if __name__ == '__main__':
+     unittest.main()
