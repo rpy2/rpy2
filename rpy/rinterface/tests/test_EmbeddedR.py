4aa9a071344430ef7ed55d148b032039d8b370f4
lgautier <none@none>
2008-08-03 09:13:04 +0000

    tests for objects dispatch (weird failure of one test while it seems to be working any time outside the tests)
    --HG--
    branch : rpy_nextgen

--- rpy/rinterface/tests/test_EmbeddedR.py
+++ rpy/rinterface/tests/test_EmbeddedR.py
@@ -20,14 +20,31 @@ class EmbeddedRTestCase(unittest.TestCase):
         self.assertEquals('[1] "3"\n', str.join('', buf))
         rinterface.setWriteConsole(rinterface.consolePrint)
 
-    def testCallWhenEndedR(self):
+    def testCallErrorWhenEndedR(self):
         t = rinterface.baseNameSpaceEnv['date']
         rinterface.endEmbeddedR(1)
         self.assertRaises(RuntimeError, t)
         rinterface.initEmbeddedR()
 
+
+class ObjectDispatchTestCase(unittest.TestCase):
+    def testObjectDispatchLang(self):
+        formula = rinterface.globalEnv.get('formula')
+        obj = formula(rinterface.StrSexpVector(['y ~ x', ]))
+        self.assertTrue(isinstance(obj, rinterface.SexpLang))
+
+    def testObjectDispatchVector(self):
+        letters = rinterface.globalEnv.get('letters')
+        self.assertTrue(isinstance(letters, rinterface.SexpVector))
+
+    def testObjectDispatchClosure(self):
+        #import pdb; pdb.set_trace()
+        help = rinterface.globalEnv.get('sum')
+        self.assertTrue(isinstance(help, rinterface.SexpClosure))
+
 def suite():
     suite = unittest.TestLoader().loadTestsFromTestCase(EmbeddedRTestCase)
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ObjectDispatchTestCase))
     return suite
 
 if __name__ == '__main__':
