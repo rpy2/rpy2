778335c2dc7156439a33f9243e92138dad2363e4
lgautier <none@none>
2008-10-31 15:39:37 +0000

    tag to 2.0.0dev
    --HG--
    branch : rpy_nextgen

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -45,8 +45,8 @@ copyright = '2008, Laurent Gautier'
 # The short X.Y version.
 version = '2.0'
 # The full version, including alpha/beta/rc tags.
-release = '2.0.0b1'
-releaselevel = 'beta'
+release = '2.0.0dev'
+releaselevel = 'dev'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup, Extension
 
 
 pack_name = 'rpy2'
-pack_version = '2.0.0b1'
+pack_version = '2.0.0dev'
 
 RHOMES = os.getenv('RHOMES')
 
