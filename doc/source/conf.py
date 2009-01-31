9be99e743734d53f18ccee556a31a5f620bf0fb6
lgautier <none@none>
2009-01-31 12:33:10 +0100

    Date and version number
    --HG--
    branch : version_2.0.x

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -37,7 +37,7 @@ master_doc = 'index'
 
 # General substitutions.
 project = 'rpy2'
-copyright = '2008, Laurent Gautier'
+copyright = '2008-2009, Laurent Gautier'
 
 # The default replacements for |version| and |release|, also used in various
 # other places throughout the built documents.
@@ -45,7 +45,7 @@ copyright = '2008, Laurent Gautier'
 # The short X.Y version.
 version = '2.0'
 # The full version, including alpha/beta/rc tags.
-release = '2.0.1'
+release = '2.0.2'
 releaselevel = ''
 
 # There are two options for replacing |today|: either, you set today to some
