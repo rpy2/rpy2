22d44d8e826ced7bc584fdf53f59b482f5774274
lgautier <none@none>
2009-02-12 20:16:25 +0100

    backport of Sexp.__deepcopy__() from branch 2.1
    --HG--
    branch : version_2.0.x

--- rpy/rinterface/rinterface.c
+++ rpy/rinterface/rinterface.c
@@ -813,6 +813,26 @@ Sexp_rsame(PyObject *self, PyObject *other)
 PyDoc_STRVAR(Sexp_rsame_doc,
              "Is the given object representing the same underlying R object as the instance.");
 
+static PyObject*
+Sexp_duplicate(PyObject *self, PyObject *kwargs)
+{
+  SEXP sexp_self, sexp_copy;
+  PyObject *res;
+  
+  sexp_self = RPY_SEXP((PySexpObject*)self);
+  if (! sexp_self) {
+    PyErr_Format(PyExc_ValueError, "NULL SEXP.");
+    return NULL;;
+  }
+  PROTECT(sexp_copy = Rf_duplicate(sexp_self));
+  res = (PyObject *) newPySexpObject(sexp_copy);
+  UNPROTECT(1);
+  return res;
+}
+PyDoc_STRVAR(Sexp_duplicate_doc,
+             "Makes a copy of the underlying Sexp object, and returns it.");
+ 
+
 
 static PyMethodDef Sexp_methods[] = {
   {"do_slot", (PyCFunction)Sexp_do_slot, METH_O,
@@ -821,6 +841,8 @@ static PyMethodDef Sexp_methods[] = {
    Sexp_do_slot_assign_doc},
   {"rsame", (PyCFunction)Sexp_rsame, METH_O,
   Sexp_rsame_doc},
+  {"__deepcopy__", (PyCFunction)Sexp_duplicate, METH_KEYWORDS,
+   Sexp_duplicate_doc},
   {NULL, NULL}          /* sentinel */
 };
 
