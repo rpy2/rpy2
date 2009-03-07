cda044ed3295765005508d923983b1c48d164b32
lgautier <none@none>
2009-03-07 20:03:30 +0100

    - Fixed case where enter is pressed on an empty line - Refresh of interactive graphics working

--- demos/radmin.py
+++ demos/radmin.py
@@ -529,15 +529,18 @@ class ConsolePanel(gtk.VBox):
 
         robjects.rinterface.setWriteConsole(f)
 
-        try:
-            res = robjects.r('withVisible( ' + rcode + ' )')
-            visible = res.r["visible"][0][0]
-            if visible:
-                res = res.r["value"][0]
-            else:
-                res = None
-        except robjects.rinterface.RRuntimeError, rre:
-            res = str(rre)
+        if rcode.strip() == '':
+            res = None
+        else:
+            try:            
+                res = robjects.r('withVisible( ' + rcode + ' )')
+                visible = res.r["visible"][0][0]
+                if visible:
+                    res = res.r["value"][0]
+                else:
+                    res = None
+            except robjects.rinterface.RRuntimeError, rre:
+                res = str(rre)
 
         if res is not None:
             self.append(str(res), "output")
@@ -653,6 +656,12 @@ gtk.window_set_auto_startup_notification(False)
 splash = get_splash()
 splash.show_all()
 gobject.idle_add(create_application, splash)
+
+def gobject_process_revents():
+    robjects.rinterface.process_revents()
+    return True
+
+gobject.idle_add(gobject_process_revents)
 gtk.main()
 
 
