76dd7eda2f2a665c1853f17a4f1798092d8b3c1f
lgautier <none@none>
2009-04-04 19:33:29 +0200

    testing rpy_device causes a segfault on linux64/python2. Tests are commented out for the time being.

--- rpy/rinterface/tests/__init__.py
+++ rpy/rinterface/tests/__init__.py
@@ -26,7 +26,7 @@ def suite():
                                    ,suite_SexpClosure
                                    ,suite_SexpVectorNumeric
                                    ,suite_EmbeddedR
-                                   ,suite_Device
+    #                               ,suite_Device
     #                               suite_EmbeddedR_multithreaded
                                   ])
     return alltests
