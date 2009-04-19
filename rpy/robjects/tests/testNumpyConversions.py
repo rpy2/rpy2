38646ac5ea71898afe4130f3a16bbcd07f24be04
lgautier <none@none>
2009-04-19 16:23:46 +0800

    Back to autofail of unit-tests for Complex and Unicode numpy objects (since they are now causing again a segfault)

--- rpy/robjects/tests/testNumpyConversions.py
+++ rpy/robjects/tests/testNumpyConversions.py
@@ -42,7 +42,7 @@ class NumpyConversionsTestCase(unittest.TestCase):
         self.checkHomogeneous(f, "numeric", "double")
 
     def testVectorComplex(self):
-        #self.assertTrue(False) # arrays of complex causing segfault
+        self.assertTrue(False) # arrays of complex causing segfault
         c = numpy.array([1j, 2j, 3j], dtype=numpy.complex_)
         self.checkHomogeneous(c, "complex", "complex")
 
@@ -51,7 +51,7 @@ class NumpyConversionsTestCase(unittest.TestCase):
         self.checkHomogeneous(s, "character", "character")
 
     def testVectorUnicodeCharacter(self):
-        #self.assertTrue(False) # arrays of unicode characters causing segfault
+        self.assertTrue(False) # arrays of unicode characters causing segfault
         u = numpy.array([u"a", u"b", u"c"], dtype="U")
         self.checkHomogeneous(u, "character", "character")
 
