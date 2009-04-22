a0f276271f256f2f7f87ee4a94933a9e60f1c1be
lgautier <none@none>
2009-04-22 10:55:50 +0800

    Hg date property added

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,4 +1,6 @@
 __version_vector__ = ((2,1,0), 'dev')
+__version_date__ = '$date$'
 
-__version__ = '.'.join([str(x) for x in __version_vector__[0]]) + '-' + __version_vector__[1]
+__version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
+              '-' + __version_vector__[1]
 
