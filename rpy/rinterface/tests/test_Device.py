7520824debca471ca3a8c334813ea7aad68f0b81
lgautier <none@none>
2009-05-16 08:10:52 +0200

    Tweaking the test for graphical device

--- rpy/rinterface/tests/test_Device.py
+++ rpy/rinterface/tests/test_Device.py
@@ -2,6 +2,7 @@ import unittest
 import rpy2.rinterface as rinterface
 import rpy2.rinterface.rpy_device as rdevice
 import sys, os, subprocess, time, tempfile, signal
+import tempfile
 
 rinterface.initr()
 
@@ -63,14 +64,15 @@ class AbstractDeviceTestCase(unittest.TestCase):
         
 class ConcreteDeviceTestCase(unittest.TestCase):
     
-    class FooDevice(rdevice.GraphicalDevice):
+    class CodeDevice(rdevice.GraphicalDevice):
         
-        def __init__(self):
-            super(ConcreteDeviceTestCase.FooDevice, self).__init__()
+        def __init__(self, filehandle):
+            super(ConcreteDeviceTestCase.CodeDevice, self).__init__()
             self._activated = None
             self._open = True
             self._pagecount = 0
-
+            self._file = filehandle
+            
         def activate(self):
             self._activated = True
 
@@ -80,33 +82,50 @@ class ConcreteDeviceTestCase(unittest.TestCase):
         def close(self):
             self._activated = None
             self._open = False
-
+            self._file.close()
+            
         def size(self, lrbt):
             return (1,2,3,4)
 
         def newpage(self):
+            self._file.write('#--- new page\n')
             self._pagecount = self._pagecount + 1
+
+        def line(self, x1, y1, x2, y2):
+            self._file.write('line(%f, %f, %f, %f)' %(x1, y1, x2, y2))
+
+        def polyline(self, x, y):
+            self._file.write('polyline(%f, %f)' %(x, y))
+            
         def clip(self):
             pass
 
     def setUp(self):
-        self.gd = ConcreteDeviceTestCase.FooDevice()
+        #f = tempfile.NamedTemporaryFile()
+        f = file('/tmp/foo', mode='w')
+        self.gd = ConcreteDeviceTestCase.CodeDevice(f)
 
     def tearDown(self):
         self.gd.close()
 
     def testActivate(self):
         self.assertTrue(self.gd._activated)
-        other_gd = ConcreteDeviceTestCase.FooDevice()
-        self.assertFalse(self.gd._activated)
+        #other_gd = ConcreteDeviceTestCase.CodeDevice()
+        #self.assertFalse(self.gd._activated)
 
     def testClose(self):
         self.gd.close()
         self.assertFalse(self.gd._open)
 
     def testSize(self):
-        pass
+        size = self.gd.size()
+        self.assertEquals(size, [1,2,3,4])
 
+    def testLine(self):
+        res = rinterface.globalEnv.get('plot.new')()
+        res = rinterface.globalEnv.get('lines')(rinterface.IntSexpVector((0, 0)),
+                                                rinterface.IntSexpVector((1, 2)))
+        
 def suite():
     suite = unittest.TestLoader().loadTestsFromTestCase(AbstractDeviceTestCase)
     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ConcreteDeviceTestCase))
