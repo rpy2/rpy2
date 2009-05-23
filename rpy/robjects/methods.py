b4f6b8dcd57f44c0ac39af62eea6a888e7660aff
lgautier <none@none>
2009-05-23 21:18:28 +0200

    added features for facilitating the declaration of Python classes mirroring RS4 classes

--- rpy/robjects/methods.py
+++ rpy/robjects/methods.py
@@ -24,8 +24,39 @@ class RS4(RObjectMixin, rinterface.SexpS4):
         return methods_env['validObject'](test = False, complete = False)
 
 
+class RS4_Type(type):
+    def __new__(mcs, name, bases, cls_dict):
+
+        cls_rname = cls_dict['__rname__']
+        for rname, where, \
+                python_name, as_property, \
+                docstring in cls_dict['__accessors__']:
+
+            if where is None:
+                where = rinterface.globalenv
+            else:
+                where = "package:" + str(where)
+                where = rinterface.StrSexpVector((where, ))
+
+            if python_name is None:
+                python_name = r_name
+                
+            signature = rinterface.StrSexpVector((cls_rname, ))            
+            r_meth = getmethod(rinterface.StrSexpVector((rname, )), 
+                               signature = signature,
+                               where = where)
+            r_meth = conversion.ri2py(r_meth)
+            if as_property:
+                cls_dict[python_name] = property(r_meth, None, None)
+            else:
+                cls_dict[python_name] =  lambda self: r_meth(self)
+                
+        return type.__new__(mcs, name, bases, cls_dict)
+
+
+
 def set_accessors(cls, cls_name, where, acs):
-    # set accessors (to be moved to metaclass ?)
+    # set accessors (to be abandonned for the metaclass above ?)
 
     if where is None:
         where = rinterface.globalenv
@@ -41,7 +72,7 @@ def set_accessors(cls, cls_name, where, acs):
                            where = where)
         r_meth = conversion.ri2py(r_meth)
         if as_property:
-            setattr(cls, python_name, property(r_meth, None, None, docstring))
+            setattr(cls, python_name, property(r_meth, None, None))
         else:
             setattr(cls, python_name, lambda self: r_meth(self))
 
--- rpy/robjects/tests/testMethods.py
+++ rpy/robjects/tests/testMethods.py
@@ -12,7 +12,31 @@ class MethodsTestCase(unittest.TestCase):
             pass
         acs = (('length', None, True, None), )
         methods.set_accessors(A, "A", None, acs)
-        self.assertTrue(False) #FIXME: no unit test
+        a = A()
+        self.assertEquals(123, a.length()[0])
+
+
+    def testRS4_TypeAccessors(self):
+        robjects.r['setClass']("A", robjects.r('list(foo="numeric")'))
+        robjects.r['setMethod']("length", signature="A",
+                                definition = robjects.r("function(x) 123"))
+        class A(methods.RS4):
+            __metaclass__ = methods.RS4_Type
+            __rname__ = 'A'
+            __slots__ = ('get_length', 'length')
+            __accessors__ = (('length', None,
+                              'get_length', False, 'get the length'),
+                             ('length', None,
+                              'length', True, 'length'))
+            def __init__(self):
+                obj = robjects.r['new']('A')
+                self.__sexp__ = obj.__sexp__
+                
+        a = A()
+        #import pdb; pdb.set_trace()
+        self.assertEquals(123, a.get_length()[0])
+        self.assertEquals(123, a.length[0])
+        
         
 def suite():
     suite = unittest.TestLoader().loadTestsFromTestCase(MethodsTestCase)
