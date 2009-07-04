9b3e9d1447b9a145dada79ea4d7c8952443c8f00
lgautier <none@none>
2009-07-04 23:55:30 +0800

    version number such as bdist_msi accepts building

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,5 +1,5 @@
-__version_vector__ = ((2,1,0), 'dev')
+__version_vector__ = ((2,1,0), 'a04072009')
 
 __version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
-              '-' + __version_vector__[1]
+              '' + __version_vector__[1]
 
