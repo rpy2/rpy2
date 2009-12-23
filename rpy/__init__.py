68b79a5822f5429e772c9a4a4cdb007089900def
lgautier <none@none>
2009-12-23 14:19:32 +0100

    version to 2.1-alpha
    --HG--
    branch : version_2.1.x

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,4 +1,4 @@
-__version_vector__ = ((2,1,0), 'dev')
+__version_vector__ = ((2,1,0), 'alpha')
 
 __version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
               '' + __version_vector__[1]
