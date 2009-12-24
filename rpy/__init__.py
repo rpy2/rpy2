52e277ff5262e8daa6b56fa8115bae3f8fddab22
lgautier <none@none>
2009-12-24 18:25:59 +0100

    version is now 2.1.0alpha1
    --HG--
    branch : version_2.1.x

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,4 +1,4 @@
-__version_vector__ = ((2,1,0), 'alpha')
+__version_vector__ = ((2,1,0), 'alpha1')
 
 __version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
               '' + __version_vector__[1]
