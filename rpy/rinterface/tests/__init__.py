a97bbdc25eaabdf281270f589cd27657db83a14e
lgautier <none@none>
2010-01-30 18:56:12 +0100

    Run verbose tests when called as __main__
    --HG--
    branch : version_2.1.x

--- rpy/rinterface/tests/__init__.py
+++ rpy/rinterface/tests/__init__.py
@@ -36,3 +36,8 @@ def main():
     r = unittest.TestResult()
     suite().run(r)
     return r
+
+if __name__ == '__main__':    
+    tr = unittest.TextTestRunner(verbosity = 2)
+    suite = suite()
+    tr.run(suite)
