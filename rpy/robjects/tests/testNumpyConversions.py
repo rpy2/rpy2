a2814666ea98cfa06e2b6b5e7a027d62fd080bc1
lgautier <none@none>
2010-03-14 08:13:56 +0100

    numpy arrays of unicode no longer segfault
    --HG--
    branch : version_2.1.x

--- rpy/robjects/tests/testNumpyConversions.py
+++ rpy/robjects/tests/testNumpyConversions.py
@@ -50,7 +50,6 @@ class NumpyConversionsTestCase(unittest.TestCase):
         self.checkHomogeneous(s, "character", "character")
 
     def testVectorUnicodeCharacter(self):
-        self.assertTrue(False) # arrays of unicode characters causing segfault
         u = numpy.array([u"a", u"b", u"c"], dtype="U")
         self.checkHomogeneous(u, "character", "character")
 
