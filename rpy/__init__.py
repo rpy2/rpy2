bcea414de5fab7f761c174d5c80c30fe7948c152
lgautier <none@none>
2010-05-16 13:58:00 +0200

    Version numbers for 2.2
    --HG--
    branch : version_2.2.x

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,4 +1,4 @@
-__version_vector__ = ((2,1,1), '')
+__version_vector__ = ((2,2,0), 'dev')
 
 __version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
               '' + __version_vector__[1]
