fe28dc7b80857fbffeed9de039adfc9f561653f5
lgautier <none@none>
2010-06-06 14:59:16 +0200

    Draft class for more extensive introspection of R classes and creation of their Python counterparts through metaclass(es)
    --HG--
    branch : version_2.2.x

--- rpy/robjects/methods.py
+++ rpy/robjects/methods.py
@@ -32,6 +32,33 @@ class RS4(RObjectMixin, rinterface.SexpS4):
         return methods_env['validObject'](self, test = test,
                                           complete = complete)[0]
 
+class ClassRepresentation(RS4):
+    """ """
+    slots = property(lambda x: [y[0] for y in x.do_slot('slots')],
+                     None, None,
+                     "Slots (attributes) for the class")
+    basenames = property(lambda x: [y[0] for y in x.do_slot('contains')],
+                     None, None,
+                     "parent classes")
+    contains = basenames
+    isabstract = property(lambda x: x.do_slot('virtual')[0],
+                          None, None,
+                          "Is the class an abstract class ?")
+    virtual = isabstract
+    packagename = property(lambda x: x.do_slot('package')[0],
+                           None, None,
+                           "R package in which the class is defined")
+    package = packagename
+    classname = property(lambda x: x.do_slot('className')[0],
+                         None, None,
+                         "Name of the R class")
+
+def getclassdef(cls_name, cls_package):
+    cls_def = methods_env['getClassDef'](rinterface.StrSexpVector((cls_name,)),
+                                         rinterface.StrSexpVector((cls_package, )))
+    cls_def = ClassRepresentation(cls_def)
+    return cls_def
+
 class RS4_Type(type):
     def __new__(mcs, name, bases, cls_dict):
 
@@ -71,6 +98,49 @@ class RS4_Type(type):
                 
         return type.__new__(mcs, name, bases, cls_dict)
 
+# playground to experiment with more metaclass-level automation
+class RS4Auto_Type(type):
+    def __new__(mcs, name, bases, cls_dict):
+        try:
+            cls_rname = cls_dict['__rname__']
+        except KeyError, ke:
+            cls_rname = name
+
+        try:
+            cls_rpackage = cls_dict['__rpackage__']
+        except KeyError, ke:
+            cls_rpackage = globalenv
+
+        cls_def = getclassdef(cls_name, cls_package)
+    
+        # documentation
+        
+        for slt_name in cls_def.slots:
+            cls_dict[slt_name] = property(lambda self: self.do_slot(slt_name),
+                                          None, None,
+                                          )
+
+            # if where is None:
+            #     where = rinterface.globalenv
+            # else:
+            #     where = "package:" + cls_def.packagename
+            #     where = rinterface.StrSexpVector((where, ))
+
+            # if python_name is None:
+            #     python_name = rname
+                
+            # signature = rinterface.StrSexpVector((cls_rname, ))            
+            # r_meth = getmethod(rinterface.StrSexpVector((rname, )), 
+            #                    signature = signature,
+            #                    where = where)
+            # r_meth = conversion.ri2py(r_meth)
+            # if as_property:
+            #     cls_dict[python_name] = property(r_meth, None, None,
+            #                                      doc = docstring)
+            # else:
+            #     cls_dict[python_name] =  lambda self: r_meth(self)
+                
+        return type.__new__(mcs, name, bases, cls_dict)
 
 
 def set_accessors(cls, cls_name, where, acs):
