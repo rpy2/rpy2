3b608c537acedba481db60aa3604f8dd340bb628
lgautier <none@none>
2010-08-08 10:38:28 +0200

    fixed benchmark for the code in R.
    --HG--
    branch : version_2.2.x

--- doc/source/_static/demos/benchmarks.py
+++ doc/source/_static/demos/benchmarks.py
@@ -4,13 +4,13 @@ import sys
 import multiprocessing
 import array, numpy, rpy2.robjects as ro
 
-
+n_loops = (10, 20, 30, 40)
 
 def setup_func(kind):
     n = 20000
     x_list = [random.random() for i in xrange(n)]
     module = None
-    if kind == "array":
+    if kind == "array.array":
         import array as module
         res = module.array('f', x_list)
     elif kind == "numpy.array":
@@ -47,6 +47,28 @@ def python_sum(x):
         total += elt
     return total
 
+
+def test_sumr(queue, func, n, setup_func):
+    array, module = setup_func("R")
+    r_loopsum = """
+function(x) { total = 0; for (elt in x) {total <- total + elt} }
+"""
+    
+    rdo_loopsum = """
+f <- %s
+for (i in 1:%i) {
+  res <- f(x)
+}
+"""
+    rcode = rdo_loopsum %(r_loopsum, n)
+    time_beg = time.time()
+    #print(rcode)
+    module.r(rcode)
+    time_end = time.time()
+    queue.put(time_end - time_beg)
+
+
+
 def test_sum(queue, func, array_type, n, setup_func):
     array, module = setup_func(array_type)
     time_beg = time.time()
@@ -56,23 +78,20 @@ def test_sum(queue, func, array_type, n, setup_func):
     queue.put(time_end - time_beg)
 
 q = multiprocessing.Queue()
-combos = [(label_func, func, label_sequence) \
+combos = [(label_func, func, label_sequence, n_loop) \
           for label_func, func in (("pure python", python_sum), \
                                    ("reduce python", python_reduce), \
                                    ("builtin python", sum))
           for label_sequence in ("SexpVector",
                                  "FloatVector",
                                  "list",
-                                 "array",
-                                 "numpy.array")]
+                                 "array.array",
+                                 "numpy.array")
+          for n_loop in n_loops]
 
 times = []
-n_loop = 100
-
-
-
 # python
-for label_func, func, label_sequence in combos:
+for label_func, func, label_sequence, n_loop in combos:
     p = multiprocessing.Process(target = test_sum, args = (q, func,
                                                            label_sequence,
                                                            n_loop,
@@ -82,47 +101,34 @@ for label_func, func, label_sequence in combos:
     p.join()
     times.append(res)
 
+times_r = []
 
 # pure R
-def test_sumr(queue, func, n, setup_func):
-    array, module = setup_func("R")
-    time_beg = time.time()
-    r_loopsum = """
-function(x) { total = 0; for (elt in x) {total <- total + elt} }
-"""
-    
-    rdo_loopsum = """
-for (i in 1:%i) {
-  res <- %s(x)
-}
-"""
-    module.r(rdo_loopsum %(n, r_loopsum))
-    time_end = time.time()
-    queue.put(time_end - time_beg)
-
-p = multiprocessing.Process(target = test_sumr, args = (q, func,
-                                                        n_loop,
-                                                        setup_func))
-p.start()
-res = q.get()
-p.join()
-times.append(res)
-combos.append(("R", None, "R"))
+for n_loop in n_loops:
+    p = multiprocessing.Process(target = test_sumr, args = (q, func,
+                                                            n_loop,
+                                                            setup_func))
+    p.start()
+    res = q.get()
+    p.join()
+    times_r.append(res)
 
 
 
-
-from rpy2.robjects.vectors import DataFrame, FloatVector, StrVector
+from rpy2.robjects.vectors import DataFrame, FloatVector, StrVector, IntVector
 d = {}
-d['code'] = StrVector([x[0] for x in combos])
-d['sequence'] = StrVector([x[-1] for x in combos])
-d['time'] = FloatVector([x for x in times])
-
+d['code'] = StrVector([x[0] for x in combos]) + StrVector(["R" for x in times_r])
+d['sequence'] = StrVector([x[-2] for x in combos]) + StrVector(["R" for x in times_r])
+d['time'] = FloatVector([x for x in times]) + FloatVector([x for x in times_r])
+d['n_loop']    = IntVector([x[-1] for x in combos]) + IntVector([x for x in n_loops])
 dataf = DataFrame(d)
+
+from rpy2.robjects import Formula
 
 from rpy2.robjects.lib import ggplot2
 p = ggplot2.ggplot(dataf) + \
-    ggplot2.geom_point(ggplot2.aes_string(x="sequence", 
-                                          y="time", 
-                                          colour="code"))
+    ggplot2.geom_line(ggplot2.aes_string(x="n_loop", 
+                                         y="time",
+                                         colour="code")) + \
+    ggplot2.facet_wrap(Formula('~sequence'))
 p.plot()
