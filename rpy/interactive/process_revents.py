d149ee6e75c8a6f10038609425cb4aec058ad1f4
lgautier <none@none>
2011-01-08 22:00:09 +0100

    RuntimeError instead of RuntimeException (as noted by Thomas)
    --HG--
    branch : version_2.2.x

--- rpy/interactive/process_revents.py
+++ rpy/interactive/process_revents.py
@@ -34,7 +34,7 @@ class EventProcessor(object):
     def start(self):
         """ start the event processor """
         if (self._thread is not None) and (self._thread.is_alive()):
-            raise RuntimeException("Processing of R events already started.")
+            raise RuntimeError("Processing of R events already started.")
         else:
             self._thread = _EventProcessorThread(name = self.name_thread)
             self._thread.setDaemon(self.daemon_thread)
