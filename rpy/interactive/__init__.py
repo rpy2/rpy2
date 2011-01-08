52a247995eaac50ed6a6619c4e3cd00d955d99d8
lgautier <none@none>
2011-01-08 22:16:23 +0100

    Committing the attempt at getting dynamic class generation for R S4 classes
    --HG--
    branch : version_2.2.x

--- rpy/interactive/__init__.py
+++ rpy/interactive/__init__.py
@@ -16,10 +16,24 @@ from rpy2.robjects.vectors import Array, Matrix
 from rpy2.robjects.vectors import StrVector
 from rpy2.robjects.vectors import ListVector, DataFrame
 from rpy2.robjects.environments import Environment
-from rpy2.robjects import Formula
+from rpy2.robjects import Formula, RS4
+from rpy2.robjects import methods
 import process_revents as revents
 
+
 class Packages(object):
+    __instance = None
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = object.__new__(cls)
+        return cls.__instance
+
+    def __setattr__(self, name, value):
+        raise AttributeError("Attributes cannot be set. Use 'importr'")
+
+class S4Classes(object):
+    """ *Very* experimental attempt at getting the S4 classes dynamically
+    mirrored. """
     __instance = None
     def __new__(cls):
         if cls.__instance is None:
@@ -38,11 +52,26 @@ def importr(packname, newname=None):
         newname = packname.replace('.', '_')
 
     Packages().__dict__[newname] = packinstance
+
+    ## Currently too slow for a serious usage: R's introspection 
+    ## of S4 classes is not fast enough
+    # d = {}
+    # for cn in methods.get_classnames(packname):
+    #     class AutoS4(RS4):
+    #         __metaclass__ = methods.RS4Auto_Type
+    #         __rpackagename__ = packname
+    #         __rname__ = cn
+    #     newcn = cn.replace('.', '_')
+    #     d[newcn] = AutoS4
+    # S4Classes().__dict__[newname] = d
+    
+
     return packinstance
 
 importr('base')
 
 packages = Packages()
+#classes = S4Classes()
 
 revents.start()
 
