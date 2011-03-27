852f106f5e45ce6909adad6056a521d1ffb00172
lgautier <none@none>
2011-03-27 21:27:40 +0200

    bump version number to 2.2.0-beta
    --HG--
    branch : version_2.2.x

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,5 +1,5 @@
 
-__version_vector__ = ((2,2,0), 'alpha4')
+__version_vector__ = ((2,2,0), 'beta')
 
 __version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
               '' + __version_vector__[1]
