d2ffe46671530ba1716c4b0f8aac136a54bff58c
lgautier@gmail.com <lgautier@gmail.com>
2013-03-09 01:33:19 +0100

    Fixed issue with setting row names and column names at the robjects level (probable internal change in R when setting slots, with a copy created where there was no copy before)
    --HG--
    branch : version_2.3.x

--- rpy/robjects/lib/tests/__init__.py
+++ rpy/robjects/lib/tests/__init__.py
@@ -1,12 +1,12 @@
 import unittest
 
-#import test_ggplot2
+import test_ggplot2
 
 def suite():
-    #suite_ggplot2 = test_ggplot2.suite()
-    #alltests = unittest.TestSuite([suite_ggplot2, ])
-    #return alltests
-    pass
+    suite_ggplot2 = test_ggplot2.suite()
+    alltests = unittest.TestSuite([suite_ggplot2, ])
+    return alltests
+    #pass
 
 def main():
     r = unittest.TestResult()
