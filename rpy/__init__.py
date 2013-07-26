1d4b978ad34d893a3c163f75bf79e5a56f46ef03
Laurent Gautier <lgautier@gmail.com>
2013-07-26 23:26:43 +0200

    Change of plan regarding rpy2-2.4.0: - the branch "default" will become version 2.5.0 - the branch "version_2.3.x" will be the starting point the the version 2.4.0
    --HG--
    branch : version_2.4.x

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,5 +1,5 @@
 
-__version_vector__ = ((2,3,7), '')
+__version_vector__ = ((2,4,0), '')
 
 __version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
               '' + __version_vector__[1]
