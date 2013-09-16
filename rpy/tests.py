d68c33539a0fa6019da1a2853c083f686de78f8b
Laurent Gautier <lgautier@gmail.com>
2013-09-15 22:13:28 -0400

    Fix for PR #147
    --HG--
    branch : version_2.4.x

--- rpy/tests.py
+++ rpy/tests.py
@@ -4,6 +4,7 @@
 
 The goal is that "ERRORS" and "FAILURES" are true failures, and expected
 problems sould be dealt with using decorators.'''
+from __future__ import print_function
 
 from os.path import dirname
 import unittest
@@ -52,11 +53,28 @@ def main(verbosity=1):
 
 if __name__ == "__main__":
     import sys, rpy2.rinterface
-    sys.stdout.write("rpy2 version: %s\n" % rpy2.__version__)
-    sys.stdout.write("built against R version: %s\n" % '-'.join(str(x)
-        for x in rpy2.rinterface.R_VERSION_BUILD))
-    sys.stdout.flush()
+    print("rpy2 version: %s" % rpy2.__version__)
+    print("- built against R version: %s" % '-'.join(str(x)
+          for x in rpy2.rinterface.R_VERSION_BUILD))
 
+    try:
+        import rpy2.rinterface
+    except Exception as e:
+        print("'rpy2.rinterface' could not be imported:")
+        print(e)
+        sys.exit(1)
+    try:
+        rpy2.rinterface.initr()
+    except Exception as e:
+        print("- The embedded R could not be initialized")
+        print(e)
+        sys.exit(1)
+    try:
+        rv = rpy2.rinterface.baseenv['R.version.string']
+        print("- running linked to R version: %s" % rv[0])
+    except KeyError as ke:
+        print("The R version dynamically linked cannot be identified.")
+        
     if len(sys.argv) > 1:
         main(verbosity=2)
     else:
