d86a57ca3ce07ae6f36fb3b3ab04ff7ae30d2a5c
Laurent Gautier <lgautier@gmail.com>
2013-09-22 21:15:03 -0400

    Lazy evaluation of __doc__ through a custom property.
    --HG--
    branch : version_2.4.x

--- rpy/robjects/functions.py
+++ rpy/robjects/functions.py
@@ -9,8 +9,6 @@ baseenv_ri = rinterface.baseenv
 
 #needed to avoid circular imports
 _reval = rinterface.baseenv['eval']
-NULL = _reval(rinterface.parse("NULL"))
-
 
 __formals = baseenv_ri.get('formals')
 __args = baseenv_ri.get('args')
@@ -20,32 +18,55 @@ def _formals_fixed(func):
     tmp = __args(func)
     return __formals(tmp)
 
+## docstring_property and DocstringProperty
+## from Bradley Froehle
+## https://gist.github.com/bfroehle/4041015
+def docstring_property(class_doc):
+    def wrapper(fget):
+        return DocstringProperty(class_doc, fget)
+    return wrapper
+ 
+class DocstringProperty(object):
+    def __init__(self, class_doc, fget):
+        self.fget = fget
+        self.class_doc = class_doc
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self.class_doc
+        else:
+            return self.fget(obj)
+    def __set__(self, obj, value):
+        raise AttributeError("Cannot set the attribute")
+    def __delete__(self, obj):
+        raise AttributeError("Cannot delete the attribute")
+
 class Function(RObjectMixin, rinterface.SexpClosure):
     """ Python representation of an R function.
     """
-
     __local = baseenv_ri.get('local')
     __call = baseenv_ri.get('call')
     __assymbol = baseenv_ri.get('as.symbol')
     __newenv = baseenv_ri.get('new.env')
 
     _local_env = None
+        
+    def __init__(self, *args, **kwargs):
+        super(Function, self).__init__(*args, **kwargs)
+        self._local_env = self.__newenv(hash=rinterface.BoolSexpVector((True, )))
 
-    def _get_doc(self):
+    @docstring_property(__doc__)
+    def __doc__(self):
         fm = _formals_fixed(self)
         doc = list(['Python representation of an R function.',
                     'R arguments:', ''])
+        if fm is rinterface.NULL:
+            doc.append('<No information available>')
         for key, val in zip(fm.do_slot('names'), fm):
             if key == '...':
                 val = 'R ellipsis (any number of parameters)'
             doc.append('%s: %s' % (key, repr(val)))
         return os.linesep.join(doc)
 
-    __doc__ = property(_get_doc)
-
-    def __init__(self, *args, **kwargs):
-        super(Function, self).__init__(*args, **kwargs)
-        self._local_env = self.__newenv(hash=rinterface.BoolSexpVector((True, )))
 
     def __call__(self, *args, **kwargs):
         new_args = [conversion.py2ri(a) for a in args]
@@ -75,18 +96,6 @@ class SignatureTranslatedFunction(Function):
     the character '.' is replaced with '_' in the R arguments names. """
     _prm_translate = None
 
-    def _get_doc(self):
-        fm = _formals_fixed(self)
-        doc = list(['Python representation of an R function.',
-                    'R arguments:', ''])
-        for key, val in self._prm_translate.items():
-            if key == '___':
-                val = 'R ellipsis (any number of parameters)'
-            doc.append('%s: %s' % (key, repr(val)))
-        return os.linesep.join(doc)
-
-    __doc__ = property(_get_doc)
-
 
     def __init__(self, sexp, init_prm_translate = None):
         super(SignatureTranslatedFunction, self).__init__(sexp)
@@ -96,18 +105,30 @@ class SignatureTranslatedFunction(Function):
             assert isinstance(init_prm_translate, dict)
             prm_translate = init_prm_translate
 
-        for r_param in self.formals().names:
-            py_param = r_param.replace('.', '_')
-            if py_param in prm_translate:
-                raise ValueError("Error: '%s' already in the transalation table" %r_param)
-            #FIXME: systematically add the parameter to the translation, as it makes it faster for generating
-            # dynamically the pydoc string from the R help.
-            #if py_param != r_param:
-            #    prm_translate[py_param] = r_param
-            prm_translate[py_param] = r_param
+        formals = self.formals()
+        if formals is not rinterface.NULL:
+            for r_param in formals.names:
+                py_param = r_param.replace('.', '_')
+                if py_param in prm_translate:
+                    raise ValueError("Error: '%s' already in the transalation table" %r_param)
+                #FIXME: systematically add the parameter to the translation, as it makes it faster for generating
+                # dynamically the pydoc string from the R help.
+                #if py_param != r_param:
+                #    prm_translate[py_param] = r_param
+                prm_translate[py_param] = r_param
         self._prm_translate = prm_translate
         if hasattr(sexp, '__rname__'):
             self.__rname__ = sexp.__rname__
+
+    @docstring_property(__doc__)
+    def __doc__(self):
+        doc = list(['Python representation of an R function.',
+                    'R arguments:', ''])
+        for key, val in self._prm_translate.items():
+            if key == '___':
+                val = 'R ellipsis (any number of parameters)'
+            doc.append('%s: %s' % (key, repr(val)))
+        return os.linesep.join(doc)
 
     def __call__(self, *args, **kwargs):
         prm_translate = self._prm_translate
