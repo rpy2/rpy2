a4d4918ab0415351b18ef513d58b9f8f69837a20
Laurent Gautier <lgautier@gmail.com>
2013-11-15 07:52:40 -0500

    default is for 2.5.0-dev

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,4 +1,4 @@
-__version_vector__ = ((2,4,0), 'dev')
+__version_vector__ = ((2,5,0), 'dev')
 
 __version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
               '' + __version_vector__[1]
