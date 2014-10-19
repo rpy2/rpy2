9cff16663491c424c3315321a1db533a2fc92703
Laurent Gautier <lgautier@gmail.com>
2014-10-19 19:15:49 -0400

    Tweaks and fixes in the HTML output for the ipython notebook

--- rpy/ipython/html.py
+++ rpy/ipython/html.py
@@ -4,7 +4,9 @@ from IPython.display import HTML
 css = """
 <style>
 .rpy2_table {
-  border: solid 1px rgb(180, 180, 180);
+  border: none;
+  border-right: solid 1px rgb(180, 180, 180);
+  border-bottom: solid 1px rgb(180, 180, 180);
   border-radius: 4px;
   -moz-border-radius: 4px;
 }
@@ -12,36 +14,50 @@ css = """
   background-color: rgb(215, 215, 215);
   border-top: none;
 }
-.rpy2_table td {
+.rpy2_table th:first-child {
+  background-color: rgb(215, 215, 215);
+  border-top: none;
+}
+
+.rpy2_table tbody tr td {
   text-align: right;
   font-family: monospace;
 }
-.rpy2_table td:first-child {
+
+.rpy_table tbody tr td:first_child {
+  border-left: solid 1px rgb(180, 180, 180);
+}
+
+.rpy2_table tbody tr td.rpy2_rowname {
   border-left: none;
+  color: rgb(180, 180, 180);
 }
 </style>
 """
 
 template_list = jinja2.Template("""
 <p><emph>{{ clsname }}</emph> with {{ rlist | length }} elements:</p>
-<ul class="rpy2">
+<dl class="rpy2">
 {%- for elt_i in range(display_neltmax) %}
-      <li><b>{{ rlist.names[elt_i] }} :</b> {{ rlist[elt_i] }}</li>
+      <dt>{{ rlist.names[elt_i] }}</dt>
+      <dd>{{ rlist[elt_i] }}</dd>
 {%- endfor %}
 {%- if display_neltmax < (rlist | length) %}
-      <li>...</li>  
+      <dt>...</dt>
+      <dd></dd>
 {%- endif %}
-</ul>
+</dl>
 """)
 
-template_vector = jinja2.Template(css+"""
+template_vector_horizontal = jinja2.Template("""
+<emph>{{ clsname }}</emph> with {{ vector | length }} elements:
 <table class="rpy2_table">
 <thead>
 </thead>
 <tbody>
 <tr>
-  {%- for elt_i in range(display_ncolmax - 2) %}
-  <td>{{ vector[elt_i]}}</td>
+  {%- for elt_i in range(display_ncolmax - size_tail) %}
+  <td>{{ vector[elt_i] }}</td>
   {%- endfor %}
   {%- if display_ncolmax < (vector | length) %}
   <td>...</td>  
@@ -54,12 +70,42 @@ template_vector = jinja2.Template(css+"""
 </table>
 """)
 
-template_dataframe = jinja2.Template(css+"""
-<emph>{{ clsname }}</emph> with {{ dataf | length }} columns:
+template_vector_vertical = jinja2.Template("""
+<emph>{{ clsname }}</emph> with {{ vector | length }} elements:
+<table class="rpy2_table">
+<thead>
+</thead>
+<tbody>
+  {%- for elt_i in range(display_nrowmax - size_tail) %}
+  <tr>
+    <td class="rpy2_rowname">{{ elt_i }}</td>
+    <td>{{ vector[elt_i]}}</td>
+  </tr>
+  {%- endfor %}
+  {%- if display_nrowmax < (vector | length) %}
+  <tr>
+    <td class="rpy2_rowname">...</td>
+    <td>...</td>  
+  </tr>
+  {%- endif %}
+  {%- for elt_i in elt_i_tail %}
+  <tr>
+    <td class="rpy2_rowname">{{ elt_i }}</td>
+    <td>{{ vector[elt_i] }}</td>
+  </tr>
+  {%- endfor %}
+</tr>
+</tbody>
+</table>
+""")
+
+template_dataframe = jinja2.Template("""
+<emph>{{ clsname }}</emph> with {{ dataf.nrow }} rows and {{ dataf | length }} columns:
 <table class="rpy2_table">
   <thead>
     <tr>
-{%- for col_i in range(display_ncolmax - 2) %}
+      <th></th>
+{%- for col_i in range(display_ncolmax - size_coltail) %}
       <th>{{ dataf.names[col_i] }}</th>
 {%- endfor %}
 {%- if display_ncolmax < dataf.ncol %}
@@ -71,9 +117,10 @@ template_dataframe = jinja2.Template(css+"""
     </tr>
   </thead>
   <tbody>
-{%- for row_i in range(display_nrowmax - 3) %}
+{%- for row_i in range(display_nrowmax - size_rowtail) %}
     <tr>
-  {%- for col_i in range(display_ncolmax - 2) %}
+      <td class="rpy2_rowname">{{ row_i }}</td>
+  {%- for col_i in range(display_ncolmax - size_coltail) %}
       <td>{{ dataf[col_i][row_i] }}</td>
   {%- endfor %}
   {%- if display_ncolmax < dataf.ncol %}
@@ -87,7 +134,8 @@ template_dataframe = jinja2.Template(css+"""
 
 {%- if dataf.nrow > display_nrowmax %}
     <tr>
-  {%- for col_i in range(display_ncolmax - 2) %}
+      <td class="rpy2_rowname">...</td>
+  {%- for col_i in range(display_ncolmax - size_coltail) %}
       <td>...</td>
   {%- endfor %}
   {%- if display_ncolmax < dataf.ncol %}
@@ -101,7 +149,8 @@ template_dataframe = jinja2.Template(css+"""
 
 {%- for row_i in row_i_tail %}
     <tr>
-  {%- for col_i in range(display_ncolmax - 2) %}
+      <td class="rpy2_rowname">{{ row_i }}</td>
+  {%- for col_i in range(display_ncolmax - size_coltail) %}
       <td>{{ dataf[col_i][row_i] }}</td>
   {%- endfor %}
   {%- if display_ncolmax < dataf.ncol %}
@@ -157,28 +206,41 @@ from rpy2.robjects import (vectors,
                            SignatureTranslatedFunction,
                            RS4)
 
-def html_vector(vector):
-    html = template_vector.render({
+def html_vector_horizontal(vector,
+                display_ncolmax=10,
+                size_tail=2):
+    html = template_vector_horizontal.render({
         'clsname': type(vector).__name__,
         'vector': vector,
-        'display_ncolmax': min(10, len(vector)),
-        'elt_i_tail': range(max(0, len(vector)-2), len(vector))})
-    return html
-def html_rlist(rlist):
-    html = template_list.render({
-        'clsname': type(rlist).__name__,
-        'rlist': rlist,
-        'display_neltmax': min(10, len(rlist))})
+        'display_ncolmax': min(display_ncolmax, len(vector)),
+        'size_tail': size_tail,
+        'elt_i_tail': range(max(0, len(vector)-size_tail), len(vector))})
     return html
 
-def html_rdataframe(dataf):
+def html_rlist(vector,
+               display_nrowmax=10,
+               size_tail=2):
+    html = template_vector_vertical.render({
+        'clsname': type(vector).__name__,
+        'vector': vector,
+        'display_nrowmax': min(display_nrowmax, len(vector)),
+        'size_tail': size_tail,
+        'elt_i_tail': range(max(0, len(vector)-size_tail), len(vector))})
+    return html
+
+def html_rdataframe(dataf,
+                    display_nrowmax=10,
+                    display_ncolmax=6,
+                    size_coltail=2, size_rowtail=2):
     html = template_dataframe.render(
         {'dataf': dataf,
          'clsname': type(dataf).__name__,
-         'display_nrowmax': min(10, dataf.nrow),
-         'display_ncolmax': min(6, dataf.ncol),
-         'col_i_tail': range(max(0, dataf.ncol-2), dataf.ncol),
-         'row_i_tail': range(max(0, dataf.nrow-2), dataf.nrow)
+         'display_nrowmax': min(display_nrowmax, dataf.nrow),
+         'display_ncolmax': min(display_ncolmax, dataf.ncol),
+         'col_i_tail': range(max(0, dataf.ncol-size_coltail), dataf.ncol),
+         'row_i_tail': range(max(0, dataf.nrow-size_rowtail), dataf.nrow),
+         'size_coltail': size_coltail,
+         'size_rowtail': size_rowtail
      })
     return html
 
@@ -227,11 +289,10 @@ def html_rs4(obj):
     html = template_rs4.render(d)
     return html
 
-
 def init_printing():
     ip = get_ipython()
     html_f = ip.display_formatter.formatters['text/html']
-    html_f.for_type(vectors.Vector, html_vector)
+    html_f.for_type(vectors.Vector, html_vector_horizontal)
     html_f.for_type(vectors.ListVector, html_rlist)
     html_f.for_type(vectors.DataFrame, html_rdataframe)
     html_f.for_type(RObject, html_ridentifiedobject)
