2f1238c846bfac5d9896f1857c242def223a133e
Laurent Gautier <lgautier@gmail.com>
2014-10-19 15:21:49 -0400

    - Bump to Numpy-1.9.0 - Attempt at trying several Python version (drone used to timeout)

--- scripts/test_drone.io.sh
+++ scripts/test_drone.io.sh
@@ -5,11 +5,11 @@
 LOGFILE=`pwd`/'ci.log'
 
 # Define the versions of Python that should be tested
-#PYTHON_VERSIONS="2.7 3.3 3.4"
-PYTHON_VERSIONS="3.3"
+PYTHON_VERSIONS="2.7 3.3 3.4"
+#PYTHON_VERSIONS="3.3"
 
 # Define the target Numpy versions
-NUMPY_VERSIONS="1.8.0"
+NUMPY_VERSIONS="1.9.0"
 
 DEPS_DIR="deps/"
 WHEEL_DIR=$DEPS_DIR"wheelhouse/"
