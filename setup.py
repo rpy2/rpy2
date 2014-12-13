356483fe6351047c8c367b7566dc0dee90268684
Laurent Gautier <lgautier@gmail.com>
2014-12-12 21:47:21 -0500

    warnings.warn(), not warnings.warnings()
    --HG--
    branch : version_2.5.x

--- setup.py
+++ setup.py
@@ -131,7 +131,7 @@ class RExec(object):
         output = output.split(os.linesep)
         #Twist if 'R RHOME' spits out a warning
         if output[0].startswith("WARNING"):
-            warnings.warning("R emitting a warning: %s" % output[0])
+            warnings.warn("R emitting a warning: %s" % output[0])
             output = output[1]
             output = output.strip()
         return output
