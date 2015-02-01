2c2fd294dfb83ed081b8d71f8ce5768b60a3422f
Laurent Gautier <lgautier@gmail.com>
2015-01-31 23:35:52 -0500

    bump version number
    --HG--
    branch : version_2.5.x

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,5 +1,5 @@
 
-__version_vector__ = ((2,5,5), '')
+__version_vector__ = ((2,5,6), '')
 
 __version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
               '' + __version_vector__[1]
