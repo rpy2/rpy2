d4e135a2f02310c147a15115bb17f7fff8b36b3a
Laurent Gautier <lgautier@gmail.com>
2015-05-09 08:56:13 -0400

    Implemented outcome of issue #274

--- rpy/robjects/packages.py
+++ rpy/robjects/packages.py
@@ -5,10 +5,14 @@ from warnings import warn
 import rpy2.rinterface as rinterface
 import rpy2.robjects.lib
 from . import conversion
-from rpy2.robjects.functions import SignatureTranslatedFunction, docstring_property, DocumentedSTFunction
+from rpy2.robjects.functions import (SignatureTranslatedFunction, 
+                                     docstring_property, 
+                                     DocumentedSTFunction)
 from rpy2.robjects.constants import NULL
 from rpy2.robjects import Environment
-from rpy2.robjects.packages_utils import _libpaths, get_packagepath, _packages
+from rpy2.robjects.packages_utils import (_libpaths, 
+                                          get_packagepath, 
+                                          _packages)
 import rpy2.robjects.help as rhelp
 
 _require = rinterface.baseenv['require']
@@ -145,11 +149,34 @@ def default_symbol_r2python(rname):
     return rname.replace('.', '_')
 
 def default_symbol_check_after(symbol_mapping):
+    # dict to store the Python symbol -> R symbols mapping causing problems.
     conflicts = dict()
-    for rpyname, rnames in symbol_mapping.items():
-        if len(rnames) > 1:
-            conflicts[rpyname] = rnames
-    return conflicts
+    resolutions = dict()
+    for py_symbol, r_symbols in symbol_mapping.items():
+        n_r_symbols = len(r_symbols)
+        if n_r_symbols == 1:
+            continue
+        elif n_r_symbols == 2:
+            # more than one R symbol associated with this Python symbol
+            try:
+                idx = r_symbols.index(py_symbol)
+                # there is an R symbol identical to the proposed Python symbol;
+                # we keep that pair mapped, and change the Python symbol for the
+                # other R symbol(s) according to PEP 0008
+                for i, s in enumerate(r_symbols):
+                    if i == idx:
+                        resolutions[py_symbol] = [s,]
+                    else:
+                        new_py_symbol = py_symbol + '_'
+                        resolutions[new_py_symbol] = [s,]
+            except ValueError:
+                # I am unsure about what to do at this point:
+                # add it as a conflict
+                conflicts[py_symbol] = r_symbols 
+        else:
+            # no automatic resolution if more than 2
+            conflicts[py_symbol] = r_symbols 
+    return conflicts, resolutions
 
 class Package(ModuleType):
     """ Models an R package
@@ -227,7 +254,7 @@ class Package(ModuleType):
             else:
                 rpyname = self._symbol_r2python(rname)
             symbol_mapping[rpyname].append(rname)
-        conflicts = self._symbol_check_after(symbol_mapping)
+        conflicts, resolutions = self._symbol_check_after(symbol_mapping)
         if len(conflicts) > 0:
             msg = ('Conflict when converting R symbols'+\
                    ' in the package "%s"' +\
@@ -249,7 +276,7 @@ class Package(ModuleType):
                 warn(msg)
             else:
                 raise ValueError('Invalid value for parameter "on_conflict"')
-
+        symbol_mapping.update(resolutions)
         for rpyname, rnames in symbol_mapping.items():
             if rpyname in self.__dict__ or rpyname == '__dict__':
                 raise LibraryError('The symbol ' + rname +\
