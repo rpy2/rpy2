7e4f2a8dd7b8397d7623b59ff4690b1196424c0e
Laurent Gautier <lgautier@gmail.com>
2015-06-07 22:57:51 -0400

    default branch is future 2.7.0

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,5 +1,5 @@
 
-__version_vector__ = ((2,6,0), '')
+__version_vector__ = ((2,7,0), 'dev')
 
 __version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
               '' + __version_vector__[1]
