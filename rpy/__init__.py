04ffbe29e3dfa37484c43ba1a985e931db3461e5
Laurent Gautier <lgautier@gmail.com>
2015-07-11 05:45:51 -0400

    correct version back after rollback
    --HG--
    branch : version_2.6.x

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,5 +1,5 @@
 
-__version_vector__ = ((2,6,1), '')
+__version_vector__ = ((2,6,1), 'dev')
 
 __version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
               '' + __version_vector__[1]
