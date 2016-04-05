61158f4f190cae4aed1c5bfa2cf464abd9a55369
Christoph Gohlke <cgohlke@uci.edu>
2016-04-04 17:15:23 -0700

    remove pywin32 dependency
    --HG--
    branch : nopywin32

--- rpy/rinterface/__init__.py
+++ rpy/rinterface/__init__.py
@@ -1,101 +1,93 @@
-import os, sys, warnings, subprocess
+import os
+import sys
+import warnings
+import subprocess
+
+if ((sys.version_info[0] == 2 and sys.version_info[1] < 7) or
+        (sys.version_info[0] == 3 and sys.version_info[1] < 3)):
+    raise RuntimeError(
+        "Python (>=2.7 and < 3.0) or >=3.3 are required to run rpy2")
+
 
 class RRuntimeWarning(RuntimeWarning):
     pass
 
-try:
-    if ((sys.version_info.major == 2) and (sys.version_info.minor < 7)) or \
-            ((sys.version_info.major == 3) and (sys.version_info.minor < 3)):
-        raise RuntimeError("Python (>=2.7 and < 3.0) or >=3.3 are required to run rpy2")
-except AttributeError:
-    # Python 2.6 and earlier do not represent version_info as
-    # a namedtuple
-    warnings.warn("Unsupported Python version. Python (>=2.7 and < 3.0) or >=3.3 are thought to be required to run rpy2.")
 
-try:
-    R_HOME = (os.environ["R_HOME"], )
-except KeyError:
-    tmp = subprocess.check_output(("R", "RHOME"), universal_newlines=True)
-    R_HOME = tmp.split(os.linesep)
-    del(tmp)
-
-if len(R_HOME) == 0:
-    if sys.platform == 'win32':
-        try:
-            import win32api
-            import win32con
-            hkey = win32api.RegOpenKeyEx(win32con.HKEY_LOCAL_MACHINE,
-                                         "Software\\R-core\\R",
-                                         0, win32con.KEY_QUERY_VALUE )
-            R_HOME = win32api.RegQueryValueEx(hkey, "InstallPath")[0]
-            win32api.RegCloseKey( hkey )
-        except ImportError(ie):
-            raise RuntimeError(
-                "No environment variable R_HOME could be found, "
-                "calling the command 'R RHOME' does not return anything, " +\
-                "and unable to import win32api or win32con, " +\
-                    "both of which being needed to retrieve where is R "+\
-                    "from the registry. You should either specify R_HOME " +\
-                    "or install the win32 package.")
-        except:
-            raise RuntimeError(
-                "No environment variable R_HOME could be found, "
-                "calling the command 'R RHOME' does not return anything, " +\
-                "and unable to determine R version from the registery." +\
-                    "This might be because R.exe is nowhere in your Path.")
+def _r_home_from_subprocess():
+    """Return the R home directory from calling 'R RHOME'."""
+    try:
+        tmp = subprocess.check_output(("R", "RHOME"), universal_newlines=True)
+    except Exception as exc:  # FileNotFoundError, WindowsError, etc
+        return
+    r_home = tmp.split(os.linesep)
+    if r_home[0].startswith("WARNING"):
+        r_home = r_home[1]
     else:
-        raise RuntimeError(
-            "R_HOME not defined, and no R command in the PATH."
-            )
-else:
-#Twist if 'R RHOME' spits out a warning
-    if R_HOME[0].startswith("WARNING"):
-        R_HOME = R_HOME[1]
-    else:
-        R_HOME = R_HOME[0]
-        R_HOME = R_HOME.strip()
+        r_home = r_home[0].strip()
+    return r_home
 
-os.environ['R_HOME'] = R_HOME
 
-# MSWindows-specific code
-_win_ok = False
+def _r_home_from_registry():
+    """Return the R home directory from the Windows Registry."""
+    try:
+        import winreg
+    except ImportError:
+        import _winreg as winreg
+    try:
+        hkey = winreg.OpenKeyEx(winreg.HKEY_LOCAL_MACHINE,
+                                "Software\\R-core\\R",
+                                0, winreg.KEY_QUERY_VALUE)
+        r_home = winreg.QueryValueEx(hkey, "InstallPath")[0]
+        winreg.CloseKey(hkey)
+    except Exception as exc:  # FileNotFoundError, WindowsError, etc
+        return
+    if sys.version_info[0] == 2:
+        r_home = r_home.encode(sys.getfilesystemencoding())
+    return r_home
+
+
+def _load_r_dll(r_home):
+    """Load the R.DLL matching Python's bitness.
+
+    Add directories containing R DLLs to the PATH environment variable.
+
+    """
+    import ctypes
+    r_arch = ('i386', 'x64')[sys.maxsize > 2**32]
+    r_mod = os.path.join(r_home, 'modules', r_arch)
+    r_bin = os.path.join(r_home, 'bin', r_arch)
+    r_dll = os.path.join(r_bin, 'R.dll')
+    if not os.path.exists(r_dll):
+        raise RuntimeError("Unable to locate R.dll at %s" % r_dll)
+    if r_bin not in os.environ.get('PATH'):
+        os.environ['PATH'] = ';'.join((os.environ.get('PATH'), r_bin, r_mod))
+    ctypes.CDLL(r_dll)
+
+
+R_HOME = os.environ.get("R_HOME")
+
+if not R_HOME:
+    R_HOME = _r_home_from_subprocess()
+
+if not R_HOME and sys.platform == 'win32':
+    R_HOME = _r_home_from_registry()
+
+if not R_HOME:
+    raise RuntimeError("""The R home directory could not be determined.
+
+    Try to install R <https://www.r-project.org/>,
+    set the R_HOME environment variable to the R home directory, or
+    add the directory of the R interpreter to the PATH environment variable.
+    """)
+
+if not os.environ.get("R_HOME"):
+    os.environ['R_HOME'] = R_HOME
+
 if sys.platform == 'win32':
-    import platform
-    architecture = platform.architecture()[0]
-    if architecture == '32bit':
-        _win_bindir = 'i386'
-    elif architecture == '64bit':
-        _win_bindir = 'x64'
-    else:
-        raise ValueError("Unknown architecture %s" %architecture)
-
-    import win32api
-    os.environ['PATH'] += ';' + os.path.join(R_HOME, 'bin', _win_bindir)
-    os.environ['PATH'] += ';' + os.path.join(R_HOME, 'modules', _win_bindir)
-    os.environ['PATH'] += ';' + os.path.join(R_HOME, 'lib')
-
-    # Load the R dll using the explicit path
-    R_DLL_DIRS = ('bin', 'lib')
-    # Try dirs from R_DLL_DIRS
-    for r_dir in R_DLL_DIRS:
-        Rlib = os.path.join(R_HOME, r_dir, _win_bindir, 'R.dll')
-        if not os.path.exists(Rlib):
-            continue
-        win32api.LoadLibrary( Rlib )
-        _win_ok = True
-        break
-    # Otherwise fail out!
-    if not _win_ok:
-        raise RuntimeError("Unable to locate R.dll within %s" % R_HOME)
-
+    _load_r_dll(R_HOME)
 
 # cleanup the namespace
 del(os)
-try:
-    del(win32api)
-    del(win32con)
-except:
-    pass
 
 from rpy2.rinterface._rinterface import (baseenv,
                                          emptyenv,
