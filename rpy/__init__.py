b76568a86b48891655f2db18d94a2374b43254b1
Laurent Gautier <lgautier@gmail.com>
2016-05-20 23:02:31 -0400

    Getting version number for release 2.8.0
    --HG--
    branch : version_2.8.x

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,5 +1,5 @@
 
-__version_vector__ = ((2,8,0), 'dev')
+__version_vector__ = ((2,8,0), '')
 
 __version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
               '' + __version_vector__[1]
