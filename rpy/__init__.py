43702eceb0f3f8988605dbf7f2a3d8221a780412
Laurent Gautier <lgautier@gmail.com>
2017-08-20 11:34:44 -0400

    bump version number for dev branch

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,5 +1,5 @@
 
-__version_vector__ = ((2,9,0), '')
+__version_vector__ = ((2,10,0), 'dev')
 
 __version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
               '' + __version_vector__[1]
