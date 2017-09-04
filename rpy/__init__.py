b3c9efcfcee2ad47ae2a78016aba3578b86634fe
Laurent Gautier <lgautier@gmail.com>
2017-09-04 10:31:58 -0400

    Next major release will be 3.0.0

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,5 +1,5 @@
 
-__version_vector__ = ((2,10,0), 'dev')
+__version_vector__ = ((3,0,0), 'dev')
 
 __version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
               '' + __version_vector__[1]
