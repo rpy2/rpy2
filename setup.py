28cc1c3394e37e2c107497438240ce83e8012111
Laurent Gautier <lgautier@gmail.com>
2017-11-04 12:41:12 -0400

    Added jinja2 to list of requirements (Fix for issue #437).
    --HG--
    branch : version_2.9.x

--- setup.py
+++ setup.py
@@ -384,7 +384,7 @@ if __name__ == '__main__':
         print("rpy2 requires at least Python Version 2.7 (with Python 3.5 or later recommended).")
         sys.exit(1)
         
-    requires=['six', ]
+    requires=['six', 'jinja2']
     if sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] < 4):
         requires.append('singledispatch')
 
