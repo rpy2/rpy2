1e5354d0a4597cbedaffe85db97c6f055cd8c05d
Laurent Gautier <lgautier@gmail.com>
2019-08-24 10:29:52 -0400

    More testing for packages_utils.

--- rpy/tests/robjects/test_packages_utils.py
+++ rpy/tests/robjects/test_packages_utils.py
@@ -68,3 +68,17 @@ def test__fix_map_symbols_invalidonconflict():
                              on_conflict,
                              msg_prefix,
                              exception)
+
+
+def test__fix_map_symbols_conflictwarn():
+    msg_prefix = ''
+    exception = ValueError
+    symbol_mappings = {'foo': 'foo'}
+    conflicts = {'foo_bar': ['foo.bar', 'foo_bar']}
+    on_conflict = 'warn'
+    with pytest.warns(UserWarning):
+        p_u._fix_map_symbols(symbol_mappings,
+                             conflicts,
+                             on_conflict,
+                             msg_prefix,
+                             exception)
