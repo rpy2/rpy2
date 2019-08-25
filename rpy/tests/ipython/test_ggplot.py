41b3abbd12c9c3b99f94a82d2c744a88306d5529
Laurent Gautier <lgautier@gmail.com>
2019-08-25 10:30:24 -0400

    Added test for ipython.ggplot

---
+++ rpy/tests/ipython/test_ggplot.py
@@ -0,0 +1,11 @@
+import pytest
+from rpy2.robjects.vectors import DataFrame
+import rpy2.robjects.lib.ggplot2
+from rpy2.ipython import ggplot
+
+
+def test_image_png():
+    dataf = DataFrame({'x': 1, 'Y': 2})
+    g = ggplot2.ggplot(dataf)
+    img = ggplot.image_png(g)
+    assert img
