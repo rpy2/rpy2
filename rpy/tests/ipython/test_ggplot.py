49650e703008ad6a902b5ca24c2038bc7f1d5e1b
Laurent Gautier <lgautier@gmail.com>
2019-08-25 10:34:02 -0400

    Fixed name path for `gglot`.

--- rpy/tests/ipython/test_ggplot.py
+++ rpy/tests/ipython/test_ggplot.py
@@ -6,6 +6,6 @@ from rpy2.ipython import ggplot
 
 def test_image_png():
     dataf = DataFrame({'x': 1, 'Y': 2})
-    g = ggplot2.ggplot(dataf)
+    g = rpy2.robjects.lib.ggplot2.ggplot(dataf)
     img = ggplot.image_png(g)
     assert img
