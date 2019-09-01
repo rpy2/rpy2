33c679d8602e9fd0eb67416a941c6125636b7b31
Laurent Gautier <lgautier@gmail.com>
2019-09-01 13:45:23 -0400

    Fixed version number (not sure how that problem appeared).

--- rpy/__init__.py
+++ rpy/__init__.py
@@ -1,6 +1,5 @@
 
 __version_vector__ = ((3,1,0), '')
 
-__version__ = '.'.join([str(x) for x in __version_vector__[0]]) + \
-              '-' + __version_vector__[1]
-
+__version__ = ('.'.join([str(x) for x in __version_vector__[0]]) +
+               __version_vector__[1])
