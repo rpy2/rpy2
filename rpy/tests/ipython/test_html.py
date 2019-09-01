23a2fcae65097bbad2125cfd4ae2c7b00f25a10f
Laurent Gautier <lgautier@gmail.com>
2019-08-31 20:22:22 -0400

    More test for HTML render in jupyter.

--- rpy/tests/ipython/test_html.py
+++ rpy/tests/ipython/test_html.py
@@ -10,8 +10,10 @@ base = importr('base')
     [(vectors.IntVector([1, 2, 3]), html.html_vector_horizontal),
      (vectors.FloatVector([1, 2, 3]), html.html_vector_horizontal),
      (vectors.StrVector(['a', 'b' 'c']), html.html_vector_horizontal),
+     (vectors.FactorVector(['a', 'b' 'c']), html.html_vector_horizontal),
      (vectors.ListVector({'a': 1, 'b': 2}), html.html_rlist),
      (vectors.DataFrame({'a': 1, 'b': 'z'}), html.html_rdataframe),
+     ('x <- c(1, 2, 3)', html.html_sourcecode),
      (base.c, html.html_ridentifiedobject)])
 def test_html_func(o, func):
     res = func(o)
