9aa355081d85dac61c7f7479566092e367d2dd9e
Laurent Gautier <lgautier@gmail.com>
2019-09-25 17:50:00 -0400

    Run ABI and API mode in Travis.
    Factored out test running to a script.

---
+++ run_tests.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+pip uninstall numpy
+pip uninstall pandas
+
+pytest \
+    --cov=rpy2.rinterface_lib \
+    --cov=rpy2.rinterface \
+    --cov=rpy2.rlike \
+    --cov=rpy2.ipython \
+    --cov=rpy2.robjects \
+    rpy2/tests
+
+pytest \
+    --cov-append \
+    --cov=rpy2.rinterface_lib.embedded \
+    rpy2/tests/rinterface/test_noinitialization.py
+
+pytest \
+    --cov-append \
+    --cov=rpy2.rinterface_lib.embedded \
+    rpy2/tests/rinterface/test_endr.py
+
+pip install numpy
+    pytest \
+    --cov-append \
+    --cov=rpy2.rinterface_lib \
+    --cov=rpy2.rinterface \
+    --cov=rpy2.rlike \
+    --cov=rpy2.ipython \
+    --cov=rpy2.robjects \
+    rpy2/tests
+
+pip install pandas
+
+pytest \
+    --cov-append \
+    --cov=rpy2.rinterface_lib \
+    --cov=rpy2.rinterface \
+    --cov=rpy2.rlike \
+    --cov=rpy2.ipython \
+    --cov=rpy2.robjects \
+    rpy2/tests
