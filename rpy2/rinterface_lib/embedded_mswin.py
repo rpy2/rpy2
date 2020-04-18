9dbd32916b697c5efbd0873962820b64eac09d54
grayver <grayver@gmail.com>
2020-04-19 00:22:58 +0300

    Windows adaption (#685)
    * Fix Windows startup parameters
    
    * Make rpy2.situation Windows-friendly
    
    * Add R_LIBS_USER to situation output
    
    
    Co-authored-by: dborovkov <dborovkov@rheosrx.com>

--- rpy2/rinterface_lib/embedded_mswin.py
+++ rpy2/rinterface_lib/embedded_mswin.py
@@ -1,9 +1,17 @@
+import sys
+import typing
 from . import embedded
 from . import callbacks
 from . import openrlib
 
 ffi = openrlib.ffi
 
+# These constants are default values from R sources
+_DEFAULT_VSIZE = 67108864  # vector heap size
+_DEFAULT_NSIZE = 350000  # language heap size
+_DEFAULT_MAX_VSIZE = sys.maxsize  # max vector heap size
+_DEFAULT_MAX_NSIZE = 50000000  # max language heap size
+_DEFAULT_PPSIZE = 50000  # stack size
 _DEFAULT_C_STACK_LIMIT = -1
 
 
@@ -12,29 +20,45 @@ def _initr_win32(
         _want_setcallbacks: bool = True,
         _c_stack_limit: int = _DEFAULT_C_STACK_LIMIT
 
-) -> int:
-    embedded.rstart = ffi.new('Rstart')
-    rstart = embedded.rstart
-    rstart.rhome = openrlib.rlib.get_R_HOME()
-    rstart.home = openrlib.rlib.getRUser()
-    rstart.CharacterMode = openrlib.rlib.LinkDLL
-    if _want_setcallbacks:
-        rstart.ReadConsole = callbacks._consoleread
-        rstart.WriteConsole = callbacks._consolewrite_ex
-        rstart.CallBack = callbacks._callback
-        rstart.ShowMessage = callbacks._showmessage
-        rstart.YesNoCancel = callbacks._yesnocancel
-        rstart.Busy = callbacks._busy
+) -> typing.Optional[int]:
+    with openrlib.rlock:
+        if embedded.isinitialized():
+            return None
 
-    rstart.R_Quiet = True
-    rstart.R_Interactive = interactive
-    rstart.RestoreAction = openrlib.rlib.SA_RESTORE
-    rstart.SaveAction = openrlib.rlib.SA_NOSAVE
+        embedded.rstart = ffi.new('Rstart')
+        rstart = embedded.rstart
+        rstart.rhome = openrlib.rlib.get_R_HOME()
+        rstart.home = openrlib.rlib.getRUser()
+        rstart.CharacterMode = openrlib.rlib.LinkDLL
+        if _want_setcallbacks:
+            rstart.ReadConsole = callbacks._consoleread
+            rstart.WriteConsoleEx = callbacks._consolewrite_ex
+            rstart.CallBack = callbacks._callback
+            rstart.ShowMessage = callbacks._showmessage
+            rstart.YesNoCancel = callbacks._yesnocancel
+            rstart.Busy = callbacks._busy
 
-    embedded.setinitialized()
+        rstart.R_Quiet = True
+        rstart.R_Interactive = interactive
+        rstart.RestoreAction = openrlib.rlib.SA_RESTORE
+        rstart.SaveAction = openrlib.rlib.SA_NOSAVE
 
-    # TODO: still needed ?
-    openrlib.rlib.R_CStackLimit = ffi.cast('uintptr_t', _c_stack_limit)
+        rstart.vsize = ffi.cast('size_t', _DEFAULT_VSIZE)
+        rstart.nsize = ffi.cast('size_t', _DEFAULT_NSIZE)
+        rstart.max_vsize = ffi.cast('size_t', _DEFAULT_MAX_VSIZE)
+        rstart.max_nsize = ffi.cast('size_t', _DEFAULT_MAX_NSIZE)
+        rstart.ppsize = ffi.cast('size_t', _DEFAULT_PPSIZE)
 
-    openrlib.rlib.setup_Rmainloop()
-    return 1
+        openrlib.rlib.R_SetParams(rstart)
+
+        options_c = [ffi.new('char[]', o.encode('ASCII'))
+                     for o in embedded._options]
+        n_options = len(options_c)
+        n_options_c = ffi.cast('int', n_options)
+        status = openrlib.rlib.Rf_initEmbeddedR(n_options_c, options_c)
+        embedded.setinitialized()
+
+        # TODO: still needed ?
+        openrlib.rlib.R_CStackLimit = ffi.cast('uintptr_t', _c_stack_limit)
+
+        return status
--- rpy2/situation.py
+++ rpy2/situation.py
@@ -145,7 +145,7 @@ def get_r_home() -> Optional[str]:
 
     if not r_home:
         r_home = r_home_from_subprocess()
-    if not r_home and sys.platform == 'win32':
+    if not r_home and os.name == 'nt':
         r_home = r_home_from_registry()
     return r_home
 
@@ -251,13 +251,19 @@ class CExtensionOptions(object):
         self.extra_link_args.extend(unknown)
 
 
-def _make_bold(text):
+def _make_bold_unix(text):
     return '%s%s%s' % ('\033[1m', text, '\033[0m')
+
+
+def _make_bold_win32(text):
+    return text
 
 
 def iter_info():
 
-    yield _make_bold('rpy2 version:')
+    make_bold = _make_bold_win32 if os.name == 'nt' else _make_bold_unix
+
+    yield make_bold('rpy2 version:')
     if has_rpy2:
         # TODO: the repeated import is needed, without which Python (3.6)
         #   raises an UnboundLocalError (local variable reference before
@@ -267,22 +273,26 @@ def iter_info():
     else:
         yield 'rpy2 cannot be imported'
 
-    yield _make_bold('Python version:')
+    yield make_bold('Python version:')
     yield sys.version
     if not (sys.version_info[0] == 3 and sys.version_info[1] >= 5):
         yield '*** rpy2 is primarily designed for Python >= 3.5'
 
-    yield _make_bold("Looking for R's HOME:")
+    yield make_bold("Looking for R's HOME:")
 
     r_home = os.environ.get('R_HOME')
     yield '    Environment variable R_HOME: %s' % r_home
 
-    if sys.platform in ('win32', 'nt'):
+    if os.name == 'nt':
         r_home_default = r_home_from_registry()
-        yield '    InstallPath in the registry: %s' % r_home
+        yield '    InstallPath in the registry: %s' % r_home_default
+        r_user = os.environ.get('R_USER')
+        yield '    Environment variable R_USER: %s' % r_user
     else:
         r_home_default = r_home_from_subprocess()
-        yield '    Calling `R RHOME`: %s' % r_home
+        yield '    Calling `R RHOME`: %s' % r_home_default
+    yield '    Environment variable R_LIBS_USER: %s' \
+          % os.environ.get('R_LIBS_USER')
 
     if r_home is not None and r_home_default is not None:
         if os.path.abspath(r_home) != r_home_default:
@@ -296,8 +306,10 @@ def iter_info():
         else:
             r_home = r_home_default
 
-    yield _make_bold("R's additions to LD_LIBRARY_PATH:")
-    yield r_ld_library_path_from_subprocess(r_home)
+    # not applicable for Windows
+    if os.name != 'nt':
+        yield make_bold("R's additions to LD_LIBRARY_PATH:")
+        yield r_ld_library_path_from_subprocess(r_home)
 
     if has_rpy2:
         try:
@@ -310,32 +322,34 @@ def iter_info():
     else:
         rlib_status = '*** rpy2 is not installed'
 
-    yield _make_bold("R version:")
+    yield make_bold("R version:")
     yield '    In the PATH: %s' % r_version_from_subprocess()
     yield '    Loading R library from rpy2: %s' % rlib_status
 
     r_libs = os.environ.get('R_LIBS')
-    yield _make_bold('Additional directories to load R packages from:')
+    yield make_bold('Additional directories to load R packages from:')
     yield r_libs
 
-    yield _make_bold('C extension compilation:')
-    c_ext = CExtensionOptions()
-    if r_home is None:
-        yield ('  Warning: R cannot be found, so no compilation flags '
-               'can be extracted.')
-    else:
-        c_ext.add_lib(*get_r_flags(r_home, '--ldflags'))
-        c_ext.add_include(*get_r_flags(r_home, '--cppflags'))
-    yield '  include:'
-    yield '  %s' % c_ext.include_dirs
-    yield '  libraries:'
-    yield '  %s' % c_ext.libraries
-    yield '  library_dirs:'
-    yield '  %s' % c_ext.library_dirs
-    yield '  extra_compile_args:'
-    yield '  %s' % c_ext.extra_compile_args
-    yield '  extra_link_args:'
-    yield '  %s' % c_ext.extra_link_args
+    # not working on Windows
+    if os.name != 'nt':
+        yield make_bold('C extension compilation:')
+        c_ext = CExtensionOptions()
+        if r_home is None:
+            yield ('  Warning: R cannot be found, so no compilation flags '
+                   'can be extracted.')
+        else:
+            c_ext.add_lib(*get_r_flags(r_home, '--ldflags'))
+            c_ext.add_include(*get_r_flags(r_home, '--cppflags'))
+        yield '  include:'
+        yield '  %s' % c_ext.include_dirs
+        yield '  libraries:'
+        yield '  %s' % c_ext.libraries
+        yield '  library_dirs:'
+        yield '  %s' % c_ext.library_dirs
+        yield '  extra_compile_args:'
+        yield '  %s' % c_ext.extra_compile_args
+        yield '  extra_link_args:'
+        yield '  %s' % c_ext.extra_link_args
 
 
 if __name__ == '__main__':
