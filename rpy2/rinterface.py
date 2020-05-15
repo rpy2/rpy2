019c3979247b4abee9e0bfa95e21282444431d93
Laurent Gautier <lgautier@gmail.com>
2020-05-14 22:46:19 -0400

    Fix for issue #700.

--- rpy2/rinterface.py
+++ rpy2/rinterface.py
@@ -530,11 +530,13 @@ class PairlistSexpVector(SexpVector):
                     ))
                 res_name = rmemory.protect(
                     rlib.Rf_allocVector(RTYPES.STRSXP, 1))
-                rlib.SET_STRING_ELT(
-                    res_name,
-                    0,
-                    rlib.PRINTNAME(rlib.TAG(item_cdata)))
-                rlib.Rf_namesgets(res_cdata, res_name)
+                item_cdata_name = rlib.PRINTNAME(rlib.TAG(item_cdata))
+                if _rinterface._TYPEOF(item_cdata_name) != rlib.NILSXP:
+                    rlib.SET_STRING_ELT(
+                        res_name,
+                        0,
+                        item_cdata_name)
+                    rlib.Rf_namesgets(res_cdata, res_name)
                 res = conversion._cdata_to_rinterface(res_cdata)
         elif isinstance(i, slice):
             iter_indices = range(*i.indices(len(self)))
