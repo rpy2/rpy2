ff39856b36322f7d3c8a0a960de675ca442f2239
Laurent Gautier <lgautier@gmail.com>
2020-05-17 11:05:20 -0400

    Linting.

--- rpy2/robjects/packages.py
+++ rpy2/robjects/packages.py
@@ -467,7 +467,9 @@ def importr(name,
     """
 
     if not isinstalled(name):
-        raise PackageNotInstalledError('The R package "%s" is not installed.' % name)
+        raise PackageNotInstalledError(
+            'The R package "%s" is not installed.' % name
+        )
 
     if suppress_messages:
         ok = quiet_require(name, lib_loc=lib_loc)
