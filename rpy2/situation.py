7632e104599269e2b0c268bd9ff36dead120977f
Laurent Gautier <lgautier@gmail.com>
2021-02-27 18:55:27 -0500

    Update situation.py

--- rpy2/situation.py
+++ rpy2/situation.py
@@ -317,9 +317,12 @@ def iter_info():
             r_home = r_home_default
 
     # not applicable for Windows
-    if r_home is not None and os.name != 'nt':
+    if os.name != 'nt':
         yield make_bold("R's additions to LD_LIBRARY_PATH:")
-        yield r_ld_library_path_from_subprocess(r_home)
+        if r_home is None:
+            yield('     *** undefined when not R home can be determined')
+        else:
+            yield r_ld_library_path_from_subprocess(r_home)
 
     if has_rpy2:
         try:
