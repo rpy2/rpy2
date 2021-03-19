3906d28c1b05e5d42550fc7fe821559b2b515141
Laurent Gautier <lgautier@gmail.com>
2021-03-18 23:18:11 -0400

    Only (re)set the signal handler for SIGINT if main thread (issue #769). (#780)
    * Only (re)set the signal handler for SIGINT if main thread (issue #769)..
    
    Co-authored-by: Laurent Gautier <lgautier@gmail.com>
    
    Co-authored-by: Michał Krassowski <5832902+krassowski@users.noreply.github.com>

--- rpy2/tests/rinterface/test_threading.py
+++ rpy2/tests/rinterface/test_threading.py
@@ -1,16 +1,43 @@
 import pytest
-from rpy2 import rinterface
+import rpy2.rinterface
+import rpy2.rinterface_lib.embedded
+from threading import Thread
 from rpy2.rinterface import embedded
 
 
-def rpy2_init():
-    from rpy2.rinterface_lib.embedded import _initr
-    _initr()
+class ThreadWithExceptions(Thread):
+    """Wrapper around Thread allowing to record exceptions from the thread."""
+
+    def run(self):
+        self.exception = None
+        try:
+            self._target()
+        except Exception as e:
+            self.exception = e
+
 
 @pytest.mark.skipif(embedded.rpy2_embeddedR_isinitialized,
                     reason='Can only be tested before R is initialized.')
-def test_threading():
-    from threading import Thread
-
-    thread = Thread(target=rpy2_init)
+def test_threading__initr():
+    thread = ThreadWithExceptions(target=rpy2.rinterface_lib.embedded._initr)
     thread.start()
+    thread.join()
+    assert not thread.exception
+
+
+@pytest.mark.skipif(embedded.rpy2_embeddedR_isinitialized,
+                    reason='Can only be tested before R is initialized.')
+def test_threading_initr_simple():
+    # This initialization performs more post-initialization setup compared to _initr.
+    # It will check whether R is initialized from the main thread.
+    thread = ThreadWithExceptions(target=rpy2.rinterface.initr_simple)
+    with pytest.warns(
+        UserWarning,
+        match=(
+            r'R is not initialized by the main thread.\n'
+            r'\W+Its taking over SIGINT cannot be reversed here.+'
+        )
+    ):
+        thread.start()
+        thread.join()
+    assert not thread.exception
