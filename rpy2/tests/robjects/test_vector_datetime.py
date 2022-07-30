22eb8360deb059d3ab5b7dbe834a93b3151aed23
Ken Williams <Ken.Williams@WindLogics.com>
2022-07-30 14:11:51 -0500

    Exercise _datetime_from_timestamp() with timezones.  Should never use dt.replace(tzinfo=tzone), see https://stackoverflow.com/q/60996205/169947

--- rpy2/tests/robjects/test_vector_datetime.py
+++ rpy2/tests/robjects/test_vector_datetime.py
@@ -131,12 +131,21 @@ def testPOSIXct_iter_localized_datetime():
 
 
 def test_POSIXct_datetime_from_timestamp():
-    tzone = robjects.vectors.get_timezone()
-    dt = [datetime.datetime(1900, 1, 1),
-          datetime.datetime(1970, 1, 1), 
-          datetime.datetime(2000, 1, 1)]
-    dt = [x.replace(tzinfo=tzone) for x in dt]
-    ts = [x.timestamp() for x in dt]
-    res = [robjects.POSIXct._datetime_from_timestamp(x, tzone) for x in ts]
-    for expected, obtained in zip(dt, res):
-        assert expected == obtained
+
+    try:
+        for zone in _zones:
+            if not zone:
+                continue
+            robjects.vectors.default_timezone = pytz.timezone(zone)
+
+            tzone = robjects.vectors.get_timezone()
+            dt = [datetime.datetime(1900, 1, 1),
+                  datetime.datetime(1970, 1, 1),
+                  datetime.datetime(2000, 1, 1)]
+            dt = [tzone.localize(x) for x in dt]
+            ts = [x.timestamp() for x in dt]
+            res = [robjects.POSIXct._datetime_from_timestamp(x, tzone) for x in ts]
+            for expected, obtained in zip(dt, res):
+                assert expected == obtained
+    finally:
+        robjects.vectors.default_timezone = None
