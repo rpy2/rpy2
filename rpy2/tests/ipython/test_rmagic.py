ccf98acc0baf2980349150cc77424ddd91b200cd
Laurent Gautier <lgautier@gmail.com>
2023-05-07 13:52:04 -0400

    Use expression in a string to avoid error if ipython not installed.

--- rpy2/tests/ipython/test_rmagic.py
+++ rpy2/tests/ipython/test_rmagic.py
@@ -125,14 +125,14 @@ def test_push_localscope(ipython_with_magic, clean_globalenv):
 
 @pytest.mark.skipif(IPython is None,
                     reason='The optional package IPython cannot be imported.')
-@pytest.mark.parametrize('rcode,exception',
-                         (('"a" + 1', rmagic.RInterpreterError),
-                          ('"a" + ', rpy2.rinterface_lib._rinterface_capi.RParsingError)))
+@pytest.mark.parametrize('rcode,exception_expr',
+                         (('"a" + 1', 'rmagic.RInterpreterError'),
+                          ('"a" + ', 'rpy2.rinterface_lib._rinterface_capi.RParsingError')))
 def test_run_cell_with_error(ipython_with_magic, clean_globalenv,
-                             rcode, exception):
+                             rcode, exception_expr):
     """Run an R block with an error."""
 
-    with pytest.raises(exception):
+    with pytest.raises(eval(exception_expr)):
         ipython_with_magic.run_line_magic('R', rcode)
 
 
