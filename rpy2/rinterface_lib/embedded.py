52b65acbcdfc7fa5f5267b82b55d0c469aea6ad5
Laurent Gautier <lgautier@gmail.com>
2023-05-07 16:32:17 -0400

    More events during initialization and termination of R logged.

--- rpy2/rinterface_lib/embedded.py
+++ rpy2/rinterface_lib/embedded.py
@@ -336,11 +336,17 @@ def endr(fatal: int) -> None:
         if rpy2_embeddedR_isinitialized & RPY_R_Status.ENDED.value:
             logger.info('Embedded R already ended.')
             return
+        logger.debug('R_do_Last()')
         rlib.R_dot_Last()
+        logger.debug('R_RunExitFinalizers()')
         rlib.R_RunExitFinalizers()
+        logger.debug('Rf_KillAllDevices()')
         rlib.Rf_KillAllDevices()
+        logger.debug('R_CleanTempDir()')
         rlib.R_CleanTempDir()
+        logger.debug('R_gc')
         rlib.R_gc()
+        logger.debug('Rf_endEmbeddedR(fatal)')
         rlib.Rf_endEmbeddedR(fatal)
         rpy2_embeddedR_isinitialized ^= RPY_R_Status.ENDED.value
         logger.info('Embedded R ended.')
