4273cf0430fedb30a4d4f0ef8b0760f333055098
Laurent Gautier <lgautier@gmail.com>
2024-11-10 14:00:55 -0500

    Add py.typed for mypy.

--- scripts/run_linting_rinterface.sh
+++ scripts/run_linting_rinterface.sh
@@ -2,7 +2,7 @@
 
 flake8 \
     --max-line-length 90 \
-    src/rpy2/situation.py \
+    src/rpy2/situation/__init__.py \
     src/rpy2/_rinterface_cffi_build.py \
     src/rpy2/rinterface/__init__.py \
     src/rpy2/rinterface_lib/
