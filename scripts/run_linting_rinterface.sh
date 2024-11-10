23283f79a035d79d5bfe098b8e9f53fcaaf20b81
Laurent Gautier <lgautier@gmail.com>
2024-11-10 11:17:04 -0500

    Paths to scripts to lint.

--- scripts/run_linting_rinterface.sh
+++ scripts/run_linting_rinterface.sh
@@ -2,7 +2,7 @@
 
 flake8 \
     --max-line-length 90 \
-    rpy2/situation.py \
-    rpy2/_rinterface_cffi_build.py \
-    rpy2/rinterface.py \
-    rpy2/rinterface_lib/
+    src/rpy2/situation.py \
+    src/rpy2/_rinterface_cffi_build.py \
+    src/rpy2/rinterface.py \
+    src/rpy2/rinterface_lib/
--- scripts/run_linting_robjects.sh
+++ scripts/run_linting_robjects.sh
@@ -2,19 +2,19 @@
 
 flake8 \
     --max-line-length 90 \
-    rpy2/ipython/ \
-    rpy2/rlike/functional.py \
-    rpy2/rlike/indexing.py \
-    rpy2/robjects/c*.py \
-    rpy2/robjects/environments.py \
-    rpy2/robjects/functions.py \
-    rpy2/robjects/help.py \
-    rpy2/robjects/language.py \
-    rpy2/robjects/lib \
-    rpy2/robjects/methods.py \
-    rpy2/robjects/numpy2ri.py \
-    rpy2/robjects/robject.py \
-    rpy2/robjects/packages.py \
-    rpy2/robjects/packages_utils.py \
-    rpy2/robjects/pandas2ri.py \
-    rpy2/robjects/vectors.py
+    src/rpy2/ipython/ \
+    src/rpy2/rlike/functional.py \
+    src/rpy2/rlike/indexing.py \
+    src/rpy2/robjects/c*.py \
+    src/rpy2/robjects/environments.py \
+    src/rpy2/robjects/functions.py \
+    src/rpy2/robjects/help.py \
+    src/rpy2/robjects/language.py \
+    src/rpy2/robjects/lib \
+    src/rpy2/robjects/methods.py \
+    src/rpy2/robjects/numpy2ri.py \
+    src/rpy2/robjects/robject.py \
+    src/rpy2/robjects/packages.py \
+    src/rpy2/robjects/packages_utils.py \
+    src/rpy2/robjects/pandas2ri.py \
+    src/rpy2/robjects/vectors.py
