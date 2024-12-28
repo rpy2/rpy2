f38df6efd1cc51d79df2df53bb006868bb80408f
Laurent Gautier <lgautier@gmail.com>
2024-12-28 17:59:52 -0500

    cffi build file moved.

--- scripts/run_linting_rinterface.sh
+++ scripts/run_linting_rinterface.sh
@@ -5,6 +5,5 @@ flake8 \
     src/rpy2/rlike/functional.py \
     src/rpy2/rlike/indexing.py \
     src/rpy2/situation/__init__.py \
-    src/rpy2/_rinterface_cffi_build.py \
     src/rpy2/rinterface/__init__.py \
     src/rpy2/rinterface_lib/
