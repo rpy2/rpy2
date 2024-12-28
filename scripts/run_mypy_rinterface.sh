d7e1c47aa88b774aef8f37a2b65a8ae206a7b3fa
Laurent Gautier <lgautier@gmail.com>
2024-12-28 18:36:51 -0500

    Build sdist for umbrella package rpy2. (#1133)
    * Build sdist for umbrella package rpy2.
    
    * Drop __init__
    
    This should not be present if namespace IIUC PEP420.
    
    * cffi build file moved.
    
    * Bump version numbers.

--- scripts/run_mypy_rinterface.sh
+++ scripts/run_mypy_rinterface.sh
@@ -4,7 +4,7 @@ MYPYPATH=src mypy \
 	     --namespace-packages \
 	     --explicit-package-bases \
 	     --exclude src/rpy2/rinterface/tests \
-	     --exclude src/rpy2/_rinterface_cffi_build.py \
+	     --exclude src/rpy2/rinterface_lib/_rinterface_cffi_build.py \
 	     --exclude build \
 	     --exclude setup.py \
 	     .
