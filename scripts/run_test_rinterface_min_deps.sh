524546eef9b8f7f3d61aeb76d7e7fa7beeabd2d2
Laurent Gautier <lgautier@gmail.com>
2025-04-25 17:29:11 -0400

    Code cleanups. (#1171)
    * Code cleanups.
    
    Add atexit function to end R.
    Remove embedded.initr_simple()
    More warnings about env variables.
    Call updated scripts for min_deps.
    Fix install target name rpy2-rinterface[test]
    Test function for threading was skipped because of incorrect name.

--- scripts/run_test_rinterface_min_deps.sh
+++ scripts/run_test_rinterface_min_deps.sh
@@ -8,10 +8,7 @@ pytest \
     --cov=rpy2.rinterface_lib \
     --cov=rpy2.rinterface \
     --cov=rpy2.rlike \
-    --cov=rpy2.ipython \
-    --cov=rpy2.robjects \
     ./rpy2-rinterface/src/rpy2/rinterface/tests \
-    ./rpy2-robjects/src/rpy2/robjects/tests
 
 pytest \
     --cov-append \
