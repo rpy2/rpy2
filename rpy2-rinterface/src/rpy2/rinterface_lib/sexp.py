ab9144598d55105da9cd48568865fb9780c9f4b0
Laurent Gautier <lgautier@gmail.com>
2025-06-05 21:23:16 -0400

    Fix duplicated method definition.

--- rpy2-rinterface/src/rpy2/rinterface_lib/sexp.py
+++ rpy2-rinterface/src/rpy2/rinterface_lib/sexp.py
@@ -749,35 +749,6 @@ class SexpVectorCCompatibleAbstract(
     def _check_C_compatible(cls, mview):
         return mview.itemsize == cls._R_SIZEOF_ELT
 
-    @classmethod
-    @_cdata_res_to_rinterface
-    def from_memoryview(cls, mview: memoryview) -> VT:
-        """Create an R vector/array from a memoryview.
-
-        The memoryview must be contiguous, and the C representation
-        for the vector must be compatible between R and Python. If
-        not the case, a :class:`ValueError` exception with will be
-        raised."""
-        if not embedded.isready():
-            raise embedded.RNotReadyError('Embedded R is not ready to use.')
-        if not mview.contiguous:
-            raise ValueError('The memory view must be contiguous.')
-        if not cls._check_C_compatible(mview):
-            cls._raise_incompatible_C_size(mview)
-
-        r_vector = None
-        n = len(mview)
-        with memorymanagement.rmemory() as rmemory:
-            r_vector = rmemory.protect(
-                openrlib.rlib.Rf_allocVector(
-                    cls._R_TYPE, n)
-            )
-            dest_ptr = cls._R_GET_PTR(r_vector)
-            src_ptr = _rinterface.ffi.from_buffer(mview)
-            nbytes = n * mview.itemsize
-            _rinterface.ffi.memmove(dest_ptr, src_ptr, nbytes)
-        return r_vector
-
 
 class SexpVector(Sexp, SexpVectorAbstract):
     """Base abstract class for R vector objects.
