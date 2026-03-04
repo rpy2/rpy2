ca297c7fd3588191f62779236c3df2da022c468a
Laurent Gautier <lgautier@gmail.com>
2026-03-03 21:45:43 -0500

    Invalid values RPY2_CFFI_MODE now raise ValueError.
    Invalid values for the environment variables no longer silently
    result in the defaut value being used.

--- rpy2-rinterface/src/rpy2/situation/__init__.py
+++ rpy2-rinterface/src/rpy2/situation/__init__.py
@@ -36,11 +36,19 @@ class CFFI_MODE(enum.Enum):
 
 def get_cffi_mode(default=CFFI_MODE.ANY):
     cffi_mode = os.environ.get(ENVVAR_CFFI_TYPE, '')
-    res = default
-    for m in (CFFI_MODE.API, CFFI_MODE.ABI,
-              CFFI_MODE.BOTH, CFFI_MODE.ANY):
-        if cffi_mode.upper() == m.value:
-            res = m
+    res = None
+    if cffi_mode:
+        for m in (CFFI_MODE.API, CFFI_MODE.ABI,
+                  CFFI_MODE.BOTH, CFFI_MODE.ANY):
+            if cffi_mode.upper() == m.value:
+                res = m
+                break
+    else:
+        res = default
+    if res is None:
+        raise ValueError(
+            f'Invalid cffi mode: {cffi_mode}'
+        )
     logger.info(f'cffi mode is {res}')
     return res
 
