ed7f745379800ee6433756aef7da384562f8626d
Laurent Gautier <lgautier@gmail.com>
2026-03-25 17:11:55 -0400

    Linting (unneed import).

--- rpy2-rinterface/src/rpy2/rinterface_lib/sexp.py
+++ rpy2-rinterface/src/rpy2/rinterface_lib/sexp.py
@@ -10,7 +10,6 @@ from rpy2.rinterface_lib import embedded
 from rpy2.rinterface_lib import memorymanagement
 from rpy2.rinterface_lib import openrlib
 import rpy2.rinterface_lib._rinterface_capi as _rinterface
-from rpy2.rinterface_lib._rinterface_capi import _evaluated_promise
 from rpy2.rinterface_lib._rinterface_capi import SupportsSEXP
 from rpy2.rinterface_lib import conversion
 from rpy2.rinterface_lib.conversion import _cdata_res_to_rinterface
